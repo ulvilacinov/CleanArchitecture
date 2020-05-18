a778afae44e70054d2e06ed65f4b15bbd922c8e3
Ülvi Laçınov <ulvi.lacin@btarge.com>
2020-05-19 02:33:14 +0300

    Initial Commit of Infrastructure.Data Project, Update Mvc for new UniversityDbContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDbContext :DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
