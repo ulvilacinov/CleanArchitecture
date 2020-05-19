5c1da57b283e3155b112a15e562119e0dba60edc
Ülvi Laçınov <ulvi.lacin@btarge.com>
2020-05-19 03:00:16 +0300

    Commit Implementation of Service and Repository GetCourses method

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _dbContext;
+        public CourseRepository(UniversityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _dbContext.Courses;
+        }
+    }
+}
